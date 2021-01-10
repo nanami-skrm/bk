31035082c25f3416ed17d27f296e27619dc5301b
unknown <skrm773@gmail.com>
2021-01-10 18:17:27 +0900

    topページ追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,6 @@
 class HomeController < ApplicationController
+
+  def top
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_10_084501) do
+ActiveRecord::Schema.define(version: 2021_01_10_085931) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "homes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

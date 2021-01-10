c259cc6cbaf21aeaf9af6238e61a2b0930e04680
unknown <skrm773@gmail.com>
2021-01-10 17:49:24 +0900

    bookコントローラー・モデル追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_09_140309) do
+ActiveRecord::Schema.define(version: 2021_01_10_084501) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
